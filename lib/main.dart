3c3df0abfe62f1e930f66d793bc8c407dc2e5da7
razeemarc <razeemarc@gmail.com>
2024-04-09 12:12:33 +0530

    Openning Page Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_text_to_speech_tutorial/pages/interactive_page.dart';
+import 'package:flutter_text_to_speech_tutorial/pages/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: WelcomeScreen(),
     );
   }
 }
